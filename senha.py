22af670d2e89387924dcc8dc147280714f9db11e
Dropeapps <123127131+Dropeapps@users.noreply.github.com>
2023-01-26 03:09:24 -0300

    Update senha.py

--- senha.py
+++ senha.py
@@ -1,22 +1,17 @@
-import streamlit as st
-
-# Cadastro de login e senha
-def cadastro():
-    usuario = st.text_input("Insira seu login: ")
-    senha = st.text_input("Insira sua senha: ", type='password')
-    st.success("Cadastro realizado com sucesso!")
-    return usuario, senha
-
-# Sistema de login
-def login():
-    usuario_cadastrado, senha_cadastrada = cadastro()
-    usuario_inserido = st.text_input("Insira seu login: ")
-    senha_inserida = st.text_input("Insira sua senha: ", type='password')
-    if usuario_inserido == usuario_cadastrado and senha_inserida == senha_cadastrada:
-        st.success("Login realizado com sucesso!")
-    else:
-        st.error("Login ou senha incorretos.")
-
 st.title("Cadastro de Login e Senha")
-st.button("Cadastrar")
-st.button("Fazer Login")
+username = st.text_input("Digite o seu nome de usuário: ")
+password = st.text_input("Digite a sua senha: ", type='password')
+
+if st.button("Cadastrar"):
+st.success("Usuário cadastrado com sucesso!")
+
+st.title("Sistema de Login")
+username_login = st.text_input("Digite o seu nome de usuário: ")
+password_login = st.text_input("Digite a sua senha: ", type='password')
+
+if st.button("Login"):
+# verifica se o login e senha digitados correspondem aos cadastrados
+if username_login == username and password_login == password:
+st.success("Login realizado com sucesso!")
+else:
+st.error("Usuário ou senha incorretos, tente novamente.")
