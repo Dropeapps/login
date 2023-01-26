2961531089790a3dc4b2830723e52d51d0195271
Dropeapps <123127131+Dropeapps@users.noreply.github.com>
2023-01-26 03:10:25 -0300

    Update senha.py

--- senha.py
+++ senha.py
@@ -3,7 +3,7 @@ username = st.text_input("Digite o seu nome de usuário: ")
 password = st.text_input("Digite a sua senha: ", type='password')
 
 if st.button("Cadastrar"):
-st.success("Usuário cadastrado com sucesso!")
+st.write("Usuário cadastrado com sucesso!")
 
 st.title("Sistema de Login")
 username_login = st.text_input("Digite o seu nome de usuário: ")
@@ -12,6 +12,6 @@ password_login = st.text_input("Digite a sua senha: ", type='password')
 if st.button("Login"):
 # verifica se o login e senha digitados correspondem aos cadastrados
 if username_login == username and password_login == password:
-st.success("Login realizado com sucesso!")
+st.write("Login realizado com sucesso!")
 else:
 st.error("Usuário ou senha incorretos, tente novamente.")
