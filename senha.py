329359580bd727a20334ec9475617d6a5b17f9b6
Dropeapps <123127131+Dropeapps@users.noreply.github.com>
2023-01-26 12:34:16 -0300

    Update senha.py

--- senha.py
+++ senha.py
@@ -1,17 +1,36 @@
-st.title("Cadastro de Login e Senha")
-username = st.text_input("Digite o seu nome de usuário: ")
-password = st.text_input("Digite a sua senha: ", type='password')
+import streamlit as st
 
+# Função para cadastrar um novo usuário
+def register():
+    username = st.text_input("Insira seu nome de usuário")
+    password = st.text_input("Insira sua senha", type='password')
+    confirm_password = st.text_input("Confirme sua senha", type='password')
+    if password != confirm_password:
+        st.error("As senhas não coincidem.")
+    else:
+        # Salvar o nome de usuário e a senha (NÃO FAÇA ISSO EM UM AMBIENTE DE PRODUÇÃO)
+        users[username] = password
+        st.success("Usuário criado com sucesso.")
+
+# Função para fazer login
+def login():
+    username = st.text_input("Insira seu nome de usuário")
+    password = st.text_input("Insira sua senha", type='password')
+    if username in users and users[username] == password:
+        st.success("Login realizado com sucesso.")
+    else:
+        st.error("Nome de usuário ou senha incorretos.")
+
+# Cria dicionario vazio para armazenar usuarios e senhas
+users = {}
+
+# Adiciona título e descrição
+st.title("Cadastro e Login")
+st.write("Cadastre-se ou faça login para continuar.")
+
+# Adiciona botões para escolher entre cadastro ou login
 if st.button("Cadastrar"):
-st.write("Usuário cadastrado com sucesso!")
-
-st.title("Sistema de Login")
-username_login = st.text_input("Digite o seu nome de usuário: ")
-password_login = st.text_input("Digite a sua senha: ", type='password')
-
+    register()
 if st.button("Login"):
-# verifica se o login e senha digitados correspondem aos cadastrados
-if username_login == username and password_login == password:
-st.write("Login realizado com sucesso!")
-else:
-st.error("Usuário ou senha incorretos, tente novamente.")
+    login()
+
